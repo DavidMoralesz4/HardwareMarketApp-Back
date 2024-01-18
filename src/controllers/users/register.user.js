597de337f688f3f18573ccd811d503e198d5ae24
Gabriel Nicolosi <gabianp@live.com.ar>
2024-01-18 15:54:16 -0300

    creación del controlador de registro de usuario, middleware para comprobar que los no falten datos desde el front para el registro

---
+++ src/controllers/users/register.user.js
@@ -0,0 +1,16 @@
+import { createUser } from '../../services/database/users.services.js';
+
+export const userRegister = async (req, res) => {
+  try {
+    const userCreated = req.user;
+
+    res.status(201).json({
+      status: 'success',
+      message: 'Registro de usuario exitoso. Inicia sesión para continuar.',
+      user: userCreated,
+    });
+  } catch (error) {
+      console.error('Register controller - Error creating user: ' + error);
+      res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -1,15 +1,8 @@
-import { Router } from "express";
-/* aqui importar los archivos de rutas */
-// import productRouter from './products.routes.js';
+import { Router } from 'express';
+import sessionRouter from './session.routes.js';
 
 const indexRouter = Router();
 
-/* aqui configurar las rutas, ej: */
-
-// indexRouter.use('/v1/api/sessions', sessionRouter);
-// indexRouter.use('/v1/api/users', userRouter);
-// indexRouter.use('/v1/api/products', productRouter);
-// indexRouter.use('/v1/api/carts', cartRouter);
-// indexRouter.use('/v1/api/payments', paymentRouter);
+indexRouter.use('/v1/api/sessions', sessionRouter);
 
 export default indexRouter;
