79f6dd8058ddd04bbd3c6ec7319734d1c09457d2
Gabriel Nicolosi <gabianp@live.com.ar>
2024-01-25 18:53:02 -0300

    create logout controller

--- src/controllers/users/logout.user.js
+++ src/controllers/users/logout.user.js
@@ -1,8 +1,14 @@
+import getLogger from '../../utils/log.utils.js';
+
+const log = getLogger();
+
 export const userLogout = (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            log.fatal('logout controller - Error al destruir la sesión', err);
-            return res.status(500).send({ error: 'Internal Server Error' });
-        }
-    })
-}
+  req.session.destroy((err) => {
+    if (err) {
+      log.fatal('logout controller - Error al destruir la sesión', err);
+      return res.status(500).send({ error: 'Internal Server Error' });
+    }
+    log.info('userLogout - Sesion finalizada');
+    res.status(200).send('Sesion finalizada');
+  });
+};
