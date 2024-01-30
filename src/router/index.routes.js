d72225cce19fe6f7f165a169e72cf4015f4962c4
Gabriel Nicolosi <gabianp@live.com.ar>
2024-01-30 16:18:26 -0300

    I create current and update users controllers with her respective routes

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import sessionRouter from './session.routes.js';
 import productsRouter from './products.routes.js';
+import userRouter from './user.routes.js';
 
 const indexRouter = Router();
 
-indexRouter.use('/v1/api/sessions', sessionRouter);
 indexRouter.use('/v1/api/products', productsRouter);
+indexRouter.use('/v1/api/sessions', sessionRouter);
+indexRouter.use('/v1/api/users', userRouter);
 
 export default indexRouter;
