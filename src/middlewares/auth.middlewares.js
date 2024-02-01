793a2becdbb2b9a8b22597f5b8ca419059464a3e
Gabriel Nicolosi <gabianp@live.com.ar>
2024-02-01 11:58:08 -0300

    I create checkRole middleware

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -16,4 +16,22 @@ const isActiveSession = (req, res, next) => {
   }
 };
 
-export { isActiveSession };
+// Middleware para verificar el rol del usuario y autorizar el acceso a ciertas rutas
+const checkRole =
+  (...requiredRole) =>
+  (req, res, next) => {
+    // Verificar si hay una sesión activa y si el usuario tiene un rol válido
+    if (
+      req.session &&
+      req.session.user &&
+      requiredRole.includes(req.session.user.role)
+    ) {
+      next();
+    } else {
+      // El usuario no tiene el rol adecuado, devolver un mensaje de error o redirigir a una página de acceso denegado
+      log.error('Acceso denegado. El usuario no tiene el rol adecuado');
+      res.status(403).send({ message: 'Acceso denegado' });
+    }
+  };
+
+export { isActiveSession, checkRole };
