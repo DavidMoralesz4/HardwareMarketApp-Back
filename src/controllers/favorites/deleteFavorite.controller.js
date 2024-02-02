75c24fce6757e15df9478addbfc1fb92023f9bea
Geovanny89 <gcasadiegosr13@gmail.com>
2024-02-02 10:22:05 -0500

    Se soluciona bug

--- src/controllers/favorites/deleteFavorite.controller.js
+++ src/controllers/favorites/deleteFavorite.controller.js
@@ -1,26 +1,27 @@
 import Favorite from '../../models/schemas/favorites.model.js';
 
-
 export const deleteFavorite = async (req, res) => {
-  const userId = req.params.fid; // Obtén el ID del usuario de los parámetros
+  const userIdFromSession = req.session.user.userId; // Obtén el ID del usuario de la sesión
+  
   const favoriteId = req.params.favid; // Obtén el ID del favorito de los parámetros
   const productIdToDelete = req.params.pid; // Obtén el ID del producto a eliminar de los parámetros
 
   try {
-    // Verifica si el favorito pertenece al usuario
-    const existingFavorite = await Favorite.findOne({ _id: favoriteId, user: userId });
+    // Verifica si el favorito pertenece al usuario de la sesión
+    const existingFavorite = await Favorite.findOne({ _id: favoriteId, user: userIdFromSession });
     if (!existingFavorite) {
       res.status(404).json({ message: 'Favorito no encontrado o no pertenece al usuario.' });
       return;
     }
 
-    // Utiliza $pull para eliminar el producto de la lista de productos
-    await Favorite.updateOne(
-      { _id: favoriteId },
-      { $pull: { products: productIdToDelete } }
+    // Utiliza findByIdAndUpdate con $pull para eliminar el producto de la lista de productos
+    const updatedFavorite = await Favorite.findByIdAndUpdate(
+      favoriteId,
+      { $pull: { products: productIdToDelete } },
+      { new: true } // Devuelve el documento actualizado
     );
 
-    res.json({ message: 'Producto eliminado del favorito exitosamente.' });
+    res.json({ message: 'Producto eliminado del favorito exitosamente.', updatedFavorite });
 
   } catch (error) {
     console.error(error);
--- src/router/favorite.routes.js
+++ src/router/favorite.routes.js
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import { addProductFavorite } from '../controllers/favorites/createFavorites.controller.js';
 import { getAllFavorite } from '../controllers/favorites/getFavoritesAll.controller.js';
 import { deleteFavorite } from '../controllers/favorites/deleteFavorite.controller.js';
+import { isActiveSession } from '../middlewares/auth.middlewares.js';
 
 
 const favoriteRouter = Router();
 
-favoriteRouter.post('/add/:fid',addProductFavorite)
+favoriteRouter.post('/add',isActiveSession,addProductFavorite)
 favoriteRouter.get('/:fid',getAllFavorite );
-favoriteRouter.delete('/:fid/:favid/:pid',deleteFavorite)
+favoriteRouter.delete('/:favid/:pid',isActiveSession,deleteFavorite)
 
 export default favoriteRouter;
