96eddc0e2b75774d77deb4499a5b67410987535c
Developer294 <juandelestal1994@gmail.com>
2024-02-09 10:23:42 -0300

    ok

--- src/config/helmet.config.js
+++ src/config/helmet.config.js
@@ -1,19 +1,21 @@
 import helmet from 'helmet';
 
 export const helmetConfig = (app) => {
-app.disable('x-powered-by')
-app.use(
-  helmet({
-    xPoweredBy: false,
-    xFrameOptions: { action: "deny" },
-    contentSecurityPolicy: {
-      directives: {
-        "script-src": ["'self'", "https://cdn.socket.io"],
+  app.disable('x-powered-by');
+  app.use(
+    helmet({
+      xPoweredBy: false,
+      xFrameOptions: { action: "deny" },
+      contentSecurityPolicy: {
+        directives: {
+          "script-src": ["'self'", "https://cdn.socket.io", "https://stackpath.bootstrapcdn.com"],
+          "style-src": ["'self'", "https://fonts.googleapis.com", "https://stackpath.bootstrapcdn.com"],
+          "font-src": ["'self'", "https://fonts.gstatic.com"]
+        },
       },
-    },
-    nosniff: true, // Config "nosniff"
-    strictTransportSecurity: false, // HSTS disabled only for work with localhost
-    xDnsPrefetchControl: { allow: false } // Disabled DNS prefetching
-  })
-);
+      nosniff: true, // Config "nosniff"
+      strictTransportSecurity: false, // HSTS disabled only for work with localhost
+      xDnsPrefetchControl: { allow: false } // Disabled DNS prefetching
+    })
+  );
 }
