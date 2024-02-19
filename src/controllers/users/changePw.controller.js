c5d053ba81d30230397b60f8e50deeb4308033ee
Developer294 <juandelestal1994@gmail.com>
2024-02-19 15:12:03 -0300

    ok

--- src/controllers/users/changePw.controller.js
+++ src/controllers/users/changePw.controller.js
@@ -19,8 +19,8 @@ export const changePw = async(req,res) => {
     if(!updatedPassword) {
        return res.status(404).json({message:'Error updating password, check all fields and try again'})
     }
-      
-    res.status(200).json({message: 'Password updated succesfully'})
+    
+    res.status(200).json({message: 'Password updated succesfully'});
   }
   catch(err) {
     res.status(500).json({message:'Internal server error'});
