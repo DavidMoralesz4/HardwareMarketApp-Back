832ffd703d00bc5704e3590bf27fa101bd1e0989
Developer294 <juandelestal1994@gmail.com>
2024-02-19 15:46:12 -0300

    Se crearon las rutas y controladores para cambiar la constraseña

--- src/controllers/mailer/sendEmail.controller.js
+++ src/controllers/mailer/sendEmail.controller.js
@@ -8,6 +8,7 @@ export const contactUs = async (req, res) => {
     try{
         const data = req.body;
         const {fromEmail, fromUser, message, subject} = data;
+        
         if(!fromEmail || !fromUser){
             res.status(400).send("Required fields are missing")
             return
@@ -17,8 +18,10 @@ export const contactUs = async (req, res) => {
         }
 
         let toEmail = config.mailer.email
+        
         await sendEmail(fromEmail, toEmail, subject, message);
         res.status(200).send({message: "Email sent succesfully"})
+    
     }catch(e){
         log.error('contactUs - Error al enviar el email de contacto.');
         return res.status(500).send({status: "error", message: "No se pudo enviar el email"})
