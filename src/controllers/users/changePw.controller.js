f5c9a692f1c7f1bc9a20b25ac4b2c0f1dd936b1a
Developer294 <juandelestal1994@gmail.com>
2024-02-19 15:32:38 -0300

    Se crearon las rutas y controladores para cambiar la constraseña

--- src/controllers/users/changePw.controller.js
+++ src/controllers/users/changePw.controller.js
@@ -8,7 +8,7 @@ export const changePw = async(req,res) => {
     if(!userEmail || newPassword || confirm){
       return res.status(400).json({message:'All fields are required'});
     }
-
+    //
     if(newPassword !== confirm){
       return res.status(400).json({message:'Passwords must be equal'});
     }
