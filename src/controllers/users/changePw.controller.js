7e70224eea970a37836bd83ba0af02e9ecb329df
Developer294 <juandelestal1994@gmail.com>
2024-02-19 13:30:16 -0300

    configuracion cambio de password completada, se agregaron dos nuevas rutas y controladores

--- src/controllers/users/changePw.controller.js
+++ src/controllers/users/changePw.controller.js
@@ -6,18 +6,19 @@ export const changePw = async(req,res) => {
   const {newPassword, confirm, userEmail} = req.body
 
   if(newPassword !== confirm) {
-    res.status(400).json({message:'Passwords don´t match, try again'})
+    return res.status(400).json({message:'Passwords don´t match, try again'})
   }
 
   const hashedPassword = await bcrypt.hash(newPassword,10);
-
-  if(hashedPassword){
-    const update = await updatePasswordByEmail(userEmail,hashedPassword);
-    if(update){
+  
+  if (hashedPassword) {
+  const update = await updatePasswordByEmail(userEmail,hashedPassword);
+    
+  if(update){
       res.status(200).json({message:'Password updated successfully'})
     }
-  }
  }
+}
  catch(err) {
   res.status(500).json({message:'Internal server error'})
   console.error(`Error updating user password ${err}`)
