30f43838c003d2c4f79224ae90ccdbc89f0bd90d
Developer294 <juandelestal1994@gmail.com>
2024-02-19 14:39:33 -0300

    configuracion cambio de password completada, se agregaron dos nuevas rutas y controladores

--- src/controllers/users/changePw.controller.js
+++ src/controllers/users/changePw.controller.js
@@ -16,7 +16,9 @@ export const sendEmailPw = async(req,res) => {
     const message = `Retrieve your HardwareMarket in the following libk: ${updatePwLink}`;
     const send = await sendEmail(config.mailer.email, userEmail, subject, message);
 
-    if(send) res.status(200).json({message: 'Email sent succesfully, don´t forget to check your spam folder'});
+    if(send) {
+     res.status(200).json({message: 'Email sent succesfully, don´t forget to check your spam folder'});
+    }
   } catch(err){
     res.status(500).json({message:'Internal server error'})
     console.error(`Error recuperando la contraseña del usuario: ${err}`)
