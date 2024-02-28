7af3c897d538aeb7c8c9889b38cd0e13c8f72fb7
Gabriel Nicolosi <gabianp@live.com.ar>
2024-02-28 19:37:28 -0300

    update in updateCart controller

--- src/controllers/carts/updateCart.controler.js
+++ src/controllers/carts/updateCart.controler.js
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import getLogger from '../../utils/log.utils.js';
 import * as cartService from '../../services/database/cart.services.js';
 import { getProductsById } from '../../services/database/product.services.js';
@@ -8,55 +7,62 @@ const log = getLogger();
 // Actualizar el carrito
 export const updateCart = async (req, res) => {
   try {
-    const  {uid}  = req.params;
+    const { uid } = req.params;
     const cid = req.params.cid;
-    const { productId, quantity } = req.body;
+    const products = req.body;
 
     // Buscar el carrito por su Id
     const cart = await cartService.getCartById(cid);
-    // console.log('Initial cart: ', cart);
     if (!cart) {
       log.warn(`Carrito con id ${cid} no encontrado`);
       return res.status(404).send({ message: 'Cart not found' });
     }
+    // Iterar sobre cada producto enviado en la solicitud
+    for (const item of products) {
+      const { productId, quantity } = item;
 
-    // Buscar el producto por su id
-    const prodFromDb = await getProductsById(productId);
-    if (!prodFromDb) {
-      log.warn(`Product ${productId} no encontrado`);
-      return res.status(404).send({ message: 'Product not found' });
-    }
+      // Buscar el producto por su id
+      const prodFromDb = await getProductsById(productId);
+      if (!prodFromDb) {
+        log.warn(`Product ${productId} no encontrado`);
+        return res.status(404).send({ message: 'Product not found' });
+      }
 
-    // console.log('userId:  ', user.userId);
-    if (prodFromDb.owner.toString() === uid) {
-      log.warn(
-        'El cliente está intentando agregar al carrito un producto de su propiedad'
-      );
-      return res.status(203).send({ message: 'Acción inválida' });
-    }
+      // Verificar si el producto pertenece al usuario
+      if (prodFromDb.owner.toString() === uid) {
+        log.warn(
+          'El cliente está intentando agregar al carrito un producto de su propiedad'
+        );
+        return res.status(203).send({ message: 'Acción inválida' });
+      }
 
-    const productInCart = cart.products.find((prod) =>
-      prod.product.equals(prodFromDb._id)
-    );
-    if (productInCart) {
-      productInCart.quantity = quantity;
-    } else {
-      cart.products.push({
-        product: new mongoose.Types.ObjectId(productId),
-        quantity: quantity,
+      // Buscar si el producto ya está en el carrito
+      const productIndex = cart.products.findIndex((prod) => {
+        return prod.product._id.toString() === productId.toString();
       });
+
+      if (productIndex !== -1) {
+        // Si el producto ya está en el carrito, actualizar la cantidad
+        cart.products[productIndex].quantity = quantity;
+      } else {
+        // Si el producto no está en el carrito, agregarlo
+        cart.products.push({
+          product: productId,
+          quantity: quantity,
+        });
+      }
     }
+    // Actualizar el carrito en la base de datos
+    const updatedCart = await cartService.updateCart(cart._id, cart.products);
 
-    const newCart = await cartService.updateCart(cart._id, cart.products);
-
-    console.log('Carrito actualizado correctamente');
     res.status(200).json({
       status: 'success',
-      message: `Se han cargado al carrito ${quantity} un. del producto ${prodFromDb.title} correctamente`,
-      data: newCart,
+      message: 'El carrito se ha actualizado correctamente',
+      data: updatedCart,
     });
   } catch (error) {
     log.fatal('Error al actualizar el carrito del usuario. ', error);
     res.status(500).send('Error interno del servidor');
   }
 };
+
